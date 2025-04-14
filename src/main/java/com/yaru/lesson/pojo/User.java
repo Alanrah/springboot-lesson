12ecd8fbbf64f98c59216b67e9be8b502a6822fc
18210623224-3u19p <18210623224@163.com>
2025-04-14 11:13:14 +0800

    新建User类,添加数据库注释,自动生成表结构

---
+++ src/main/java/com/yaru/lesson/pojo/User.java
@@ -0,0 +1,60 @@
+package com.yaru.lesson.pojo;
+
+
+import jakarta.persistence.*;
+
+// 把 User 类映射为数据库的一个表
+@Table(name="lesson_user")
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Integer userId;
+
+    @Column(name = "user_name")
+    private String userName;
+
+    private String password;
+
+    private String mobile;
+
+    // 也可以直接 @Getter @Setter ，就不用显示的设置了
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+
+}
